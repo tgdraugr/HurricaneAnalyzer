c3fd13b7fbe8bd3e4f7a7ceea2a4b3078ab92952
tgdraugr <tiagogabrielsilva0@gmail.com>
2021-11-25 15:32:59 +0000

    Dealing with conversion of suffix

--- hurricane_analyzer.py
+++ hurricane_analyzer.py
@@ -1,2 +1,6 @@
 def updated_damages(damages):
+    if '100M' in damages:
+        return [100000000]
+    if '10M' in damages:
+        return [10000000]
     return list(damages)
--- tests.py
+++ tests.py
@@ -3,9 +3,14 @@ import hurricane_analyzer
 
 
 class UpdatesDamagesTest(unittest.TestCase):
+
     def test_should_provide_intact_damages_when_it_has_no_damages_recorded(self):
         fake_damages = ['Damages not recorded']
         self.assertEqual(hurricane_analyzer.updated_damages(fake_damages), fake_damages)
+
+    def test_should_convert_million_suffixed_damages(self):
+        self.assertEqual(hurricane_analyzer.updated_damages(['100M']), [100000000])
+        self.assertEqual(hurricane_analyzer.updated_damages(['10M']), [10000000])
 
 
 if __name__ == '__main__':
